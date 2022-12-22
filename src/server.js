1439b5a56ce9c662f692f8a914230a9c4ff87288
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-12-23 00:01:11 +0100

    some updates

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import authorsRouter from "./authors/index.js";
-import {
-  genericErrorHandler,
-  notFoundHandler,
-  badRequestHandler,
-  unauthorizedHandler,
-} from "./errorHandler.js";
+// import {
+//   genericErrorHandler,
+//   notFoundHandler,
+//   badRequestHandler,
+//   unauthorizedHandler,
+// } from "./errorHandler.js";
 import cors from "cors";
 
 const server = express();
@@ -14,12 +14,23 @@ const port = 3001;
 server.use(cors());
 server.use(express.json());
 
+//   console.log("I am a middleware");
+//   if (req.query.protected) {
+//     res.status(401).send("you are not allowed here");
+//   } else {
+//     next();
+//   }
+// };
 server.use("/authors", authorsRouter);
 
-server.use(badRequestHandler); // 400
-server.use(unauthorizedHandler); // 401
-server.use(notFoundHandler); // 404
-server.use(genericErrorHandler); // 500
+// const testMiddleware = (req, res, next) => {
+
+// server.use(badRequestHandler); // 400
+// server.use(unauthorizedHandler); // 401
+// server.use(notFoundHandler); // 404
+// server.use(genericErrorHandler); // 500
+
+//TODO: Finish error handlers
 
 server.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
